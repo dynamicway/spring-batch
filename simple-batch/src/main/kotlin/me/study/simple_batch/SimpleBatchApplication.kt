5a47194d203ba1c885dd50c9582c6d0adfba277f
bee <rkddbtjd123@gmail.com>
2023-02-18 23:57:17 +0900

    apply multi gradle

--- simple-batch/src/main/kotlin/me/study/simple_batch/SimpleBatchApplication.kt
+++ simple-batch/src/main/kotlin/me/study/simple_batch/SimpleBatchApplication.kt
@@ -1,8 +1,10 @@
-package me.study.spring_batch
+package me.study.simple_batch
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@EnableBatchProcessing
 @SpringBootApplication
 class SpringBatchApplication
 
