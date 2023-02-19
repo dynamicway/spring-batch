4eeed857349d09ccd2f8fd279f3617c20274c7be
bee <rkddbtjd123@gmail.com>
2023-02-19 14:59:05 +0900

    feat: Change simple-batch module application starter object name to SimpleBatchApplication
    - Add the @EnableScheduling annotation to the same class

--- simple-batch/src/main/kotlin/me/study/simple_batch/SimpleBatchApplication.kt
+++ simple-batch/src/main/kotlin/me/study/simple_batch/SimpleBatchApplication.kt
@@ -3,11 +3,13 @@ package me.study.simple_batch
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableBatchProcessing
+@EnableScheduling
 @SpringBootApplication
-class SpringBatchApplication
+class SimpleBatchApplication
 
 fun main(args: Array<String>) {
-    runApplication<SpringBatchApplication>(*args)
+    runApplication<SimpleBatchApplication>(*args)
 }
