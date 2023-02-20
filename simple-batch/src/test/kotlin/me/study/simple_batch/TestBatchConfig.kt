655d3f16a58d612467aa87b6ef4e461018a4d318
bee <rkddbtjd123@gmail.com>
2023-02-20 17:23:34 +0900

    test: Add TestBatchConfig

---
+++ simple-batch/src/test/kotlin/me/study/simple_batch/TestBatchConfig.kt
@@ -0,0 +1,10 @@
+package me.study.simple_batch
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableAutoConfiguration
+@EnableBatchProcessing
+class TestBatchConfig
