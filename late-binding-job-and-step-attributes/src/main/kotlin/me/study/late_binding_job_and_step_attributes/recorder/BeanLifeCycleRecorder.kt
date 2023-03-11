c1f3676ebf3886bfa99d38b5e49aadd63f3d296c
bee <rkddbtjd123@gmail.com>
2023-03-12 01:08:24 +0900

    feat: Add ability to record index when recording bean lifecycle

--- late-binding-job-and-step-attributes/src/main/kotlin/me/study/late_binding_job_and_step_attributes/recorder/BeanLifeCycleRecorder.kt
+++ late-binding-job-and-step-attributes/src/main/kotlin/me/study/late_binding_job_and_step_attributes/recorder/BeanLifeCycleRecorder.kt
@@ -1,14 +1,19 @@
 package me.study.late_binding_job_and_step_attributes.recorder
 
 import org.springframework.stereotype.Component
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicLong
 import kotlin.reflect.KClass
 
 @Component
 class BeanLifeCycleRecorder {
-    private val records = mutableMapOf<KClass<*>, MutableList<BeanLifeCycle>>()
+    private val index = AtomicLong()
+    private val records = mutableMapOf<KClass<*>, ConcurrentHashMap<Long, BeanLifeCycle>>()
 
     fun record(kClass: KClass<*>, currentLifeCycle: BeanLifeCycle) = records.computeIfAbsent(kClass) { _ ->
-        mutableListOf(currentLifeCycle)
+        val records = ConcurrentHashMap<Long, BeanLifeCycle>()
+        records[index.getAndIncrement()] = currentLifeCycle
+        records
     }
 
     fun getRecords(kClass: KClass<*>) =
