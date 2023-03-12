ac2bb29a7624498448ffbfb66d74ab4e2558e03d
bee <rkddbtjd123@gmail.com>
2023-03-12 23:50:58 +0900

    feat: Add SpringBatchTest annotation to BatchTest annotation

--- late-binding-job-and-step-attributes/src/test/kotlin/me/study/late_binding_job_and_step_attributes/config/BatchTest.kt
+++ late-binding-job-and-step-attributes/src/test/kotlin/me/study/late_binding_job_and_step_attributes/config/BatchTest.kt
@@ -1,10 +1,12 @@
-package me.study.simple_batch
+package me.study.late_binding_job_and_step_attributes.config
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.batch.test.context.SpringBatchTest
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @EnableAutoConfiguration
 @EnableBatchProcessing
-class TestBatchConfig
+@SpringBatchTest
+annotation class BatchTest
